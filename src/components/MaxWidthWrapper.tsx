f1264c13d7774202ae6ca3ebd078d5d2f54df25c
aazis7 <azis190407@gmail.com>
2025-06-07 06:29:21 +0000

    fix: eslint disable rule

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { cn } from "~/lib/utils";
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface MaxWidthWrapperProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export const MaxWidthWrapper: React.FC<MaxWidthWrapperProps> = ({
